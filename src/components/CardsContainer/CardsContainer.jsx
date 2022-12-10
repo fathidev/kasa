6ead1215d8abcbf0360b5c952587a809328568b3
FathiDev <fathi.devop@gmail.com>
2022-12-10 09:26:28 +0100

    card fix overlay

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -10,15 +10,15 @@ function CardsContainer() {
           <div className="card" key={record.id}>
             <div className="card-image">
               <Link to={"products/" + record.id} className="card-link">
+                <div className="card_overlay">
+                  <img
+                    className="card-vignette"
+                    src={record.cover}
+                    alt={record.title}
+                  />
+                </div>
                 <div className="card-title">
                   <h2>{record.title}</h2>
-                </div>
-                <div className="card_overlay">
-                <img 
-                  className="card-vignette"
-                  src={record.cover}
-                  alt={record.title}
-                />
                 </div>
               </Link>
             </div>
