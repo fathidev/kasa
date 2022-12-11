460378cfcd797bd491b168a1270d98e89eff4d87
FathiDev <fathi.devop@gmail.com>
2022-12-11 16:48:47 +0100

    Renaming classes CSS

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import "./Collapse.css";
-import arrowDropdown from "../../assets/arrowDropdown.svg";
+import arrowCollapse from "../../assets/arrowCollapse.svg";
 
 function Collapse({ titre, description }) {
   const [ouvert, setOuvert] = useState(false);
 
   return (
-    <div className="dropdown" id={`dropdown-${titre}`}>
-      <div className="dropdown_header">
-        <div className="dropdown_title">{titre}</div>
+    <div className="collapse" id={`collapse-${titre}`}>
+      <div className="collapse_header">
+        <div className="collapse_title">{titre}</div>
         <span
-          className={`dropdown_arrow ${ouvert}`}
+          className={`collapse_arrow ${ouvert}`}
           onClick={() => setOuvert(!ouvert)}
         >
-          <img src={arrowDropdown} alt="Ouvrir cette liste déroulante" />
+          <img src={arrowCollapse} alt="Ouvrir cette liste déroulante" />
         </span>
       </div>
       {
-        /* Si le dropdown est à TRUE alors il affichera la description */
-        ouvert && <div className="dropdown_description">{description}</div>
+        /* Si le collapse est à TRUE alors il affichera la description */
+        ouvert && <div className="collapse_description">{description}</div>
       }
     </div>
   );
